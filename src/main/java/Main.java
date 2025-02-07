91056d583c2071f4d3f43a3fbfc656d3b4b13349
Denis <Denis@email.com>
2025-02-07 22:54:46 +1000

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        ITnavigator iTnavigator = new ITnavigator();
+    }
+}
---
+++ src/main/java/Questios.java
@@ -0,0 +1,14 @@
+public enum Questios {
+
+    questios1("");
+
+    private final String questio;
+
+    Questios(String questio) {
+        this.questio = questio;
+    }
+
+    public String getQuestion() {
+        return questio;
+    }
+}
